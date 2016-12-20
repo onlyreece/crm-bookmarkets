310de30bf907920fceed61c72e7abda64c9e5c2f
Reece Campbell <reece@glinks.co.uk>
2016-12-20 17:14:16 +0000

    New bookmarklet to enable fields

---
+++ src/EnableAllFields.js
@@ -0,0 +1,17 @@
+function  getXrmPage() {
+    for (var index = 0; index < frames.length; index++) {
+        try {
+            var controls = frames[index].Xrm.Page.ui.controls;
+            if (typeof controls === 'object')
+                return frames[index].Xrm.Page;
+        } catch (error){
+            console.debug(error);
+        }
+    }
+}
+
+var xp = getXrmPage();
+xp.ui.controls.forEach(function(c,i){
+    if (c.getDisabled() === true)
+        c.setDisabled(false);
+})
